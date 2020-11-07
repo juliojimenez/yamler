888b1c207f63c60e84d49af36c1eb4c85149c3d4
Julio Jimenez <julio.jimenez@rallyhealth.com>
2020-11-06 21:53:13 -0500

    more

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ import fs from "fs";
 import YAML from "yaml";
 
 (async () => {
-  try {
-    const yamlFilePath = core.getInput("yaml-file");
-    const yamlFile = fs.readFileSync(yamlFilePath, "utf8");
-    const yamlParse = YAML.parse(yamlFile);
-    console.log(yamlParse);
-    // const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  // try {
+  const yamlFilePath = core.getInput("yaml-file");
+  const yamlFile = fs.readFileSync(yamlFilePath, "utf8");
+  const yamlParse = YAML.parse(yamlFile);
+  console.log(yamlParse);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  // } catch (error) {
+  //   core.setFailed(error.message);
+  // }
 })();

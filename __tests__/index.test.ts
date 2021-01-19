38b5666eb72eb5ed7fb0fd97bc7767060e5658b4
Julio Jimenez <julio@julioj.com>
2021-01-19 15:56:41 -0500

    yoseph

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,7 +3,7 @@ import { safeString } from "../src/index";
 describe("index", () => {
   it("creates safe strings for github workflow output variables", () => {
     const unsafeString: string =
-      "This string is a valid YAML string, but we're going to make it safe for GitHub Workflow Output Variables";
+      "This-string.is:a valid YAML string, but we're going to make it #afe for (GitHub) [Workflow] Output/Variables";
     const result: string = safeString(unsafeString);
     console.log(result);
   });

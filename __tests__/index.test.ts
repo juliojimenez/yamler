e519eb85a9e875fb9384698e39204e2d47495539
Julio Jimenez <julio@julioj.com>
2021-01-19 16:08:09 -0500

    yoseph

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -5,6 +5,8 @@ describe("index", () => {
     const unsafeString: string =
       "This-string.is:a valid YAML string, but we're going to make it #afe for (GitHub) [Workflow] Out+ut/Variables";
     const result: string = safeString(unsafeString);
-    console.log(result);
+    expect(result).toEqual(
+      "this_string_is_a_valid_yaml_string_but_were_going_to_make_it_safe_for_github_workflow_output_variables"
+    );
   });
 });

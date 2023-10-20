6f90aef68662075b7794dd7cc813cf9dbca19f16
Julio Jimenez <julio@julioj.com>
2023-10-19 20:36:24 -0400

    let's see
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import * as core from "@actions/core"
+import * as core from '@actions/core'
 // import github from "@actions/github"
-import fs from "fs"
-import YAML from "yaml"
+import fs from 'fs'
+import YAML from 'yaml'
 
 let parentNodes: Array<string> = []
 
@@ -10,42 +10,34 @@ export function safeString(unsafeString: string): string {
   /*
     Replace whitespace OR / OR - OR . OR : WITH _
   */
-  const replaceSpacesEtc = makeLowerCase.replace(/\s|\/|-|\.|:/g, "_")
+  const replaceSpacesEtc = makeLowerCase.replace(/\s|\/|-|\.|:/g, '_')
   /*
     Replace ( OR ) OR [ OR ] OR ' OR , WITH ""
   */
-  const removeParenthesesEtc = replaceSpacesEtc.replace(/\(|\)|\[|\]|'|,/g, "")
+  const removeParenthesesEtc = replaceSpacesEtc.replace(/\(|\)|\[|\]|'|,/g, '')
   /*
     Replace + WITH p
   */
-  const replacePlus = removeParenthesesEtc.replace(/\+/g, "p")
+  const replacePlus = removeParenthesesEtc.replace(/\+/g, 'p')
   /*
     Replace # WITH s
   */
-  const replaceSharp = replacePlus.replace(/#/g, "s")
+  const replaceSharp = replacePlus.replace(/#/g, 's')
   return replaceSharp
 }
 
-export function traverseObject(
-  theObject: { [index: string]: any },
-  documentIndex: number = -1,
-): boolean {
+export function traverseObject(theObject: { [index: string]: any }, documentIndex: number = -1): boolean {
   for (let key of Object.keys(theObject)) {
     const keyType = typeof theObject[key]
-    if (
-      keyType === "string" ||
-      keyType === "number" ||
-      keyType === "boolean" ||
-      keyType === "bigint"
-    ) {
+    if (keyType === 'string' || keyType === 'number' || keyType === 'boolean' || keyType === 'bigint') {
       const keyString: string = safeString(
-        `${documentIndex < 0 ? "" : `doc${documentIndex}`}${parentNodes.join(
-          "__",
-        )}${parentNodes.length > 0 ? "__" : ""}${key}`,
+        `${documentIndex < 0 ? '' : `doc${documentIndex}`}${parentNodes.join('__')}${
+          parentNodes.length > 0 ? '__' : ''
+        }${key}`,
       )
       console.log(keyString)
       handleString(keyString, theObject[key])
-    } else if (keyType === "object") {
+    } else if (keyType === 'object') {
       parentNodes.push(safeString(key))
       if (Array.isArray(theObject[key])) {
         traverseArray(theObject[key], documentIndex)
@@ -58,28 +50,18 @@ export function traverseObject(
   return true
 }
 
-export function traverseArray(
-  theArray: Array<any>,
-  documentIndex: number = -1,
-): boolean {
+export function traverseArray(theArray: Array<any>, documentIndex: number = -1): boolean {
   for (let elem of theArray) {
     const elemType = typeof elem
-    if (
-      elemType === "string" ||
-      elemType === "number" ||
-      elemType === "boolean" ||
-      elemType === "bigint"
-    ) {
+    if (elemType === 'string' || elemType === 'number' || elemType === 'boolean' || elemType === 'bigint') {
       const keyString: string = safeString(
-        `${documentIndex < 0 ? "" : `doc${documentIndex}`}${parentNodes.join(
-          "__",
-        )}${parentNodes.length > 0 ? "__" : ""}${String(
-          theArray.indexOf(elem),
-        )}`,
+        `${documentIndex < 0 ? '' : `doc${documentIndex}`}${parentNodes.join('__')}${
+          parentNodes.length > 0 ? '__' : ''
+        }${String(theArray.indexOf(elem))}`,
       )
       console.log(keyString)
       handleString(keyString, elem)
-    } else if (elemType === "object") {
+    } else if (elemType === 'object') {
       parentNodes.push(String(theArray.indexOf(elem)))
       if (Array.isArray(elem)) {
         traverseArray(elem, documentIndex)
@@ -99,9 +81,9 @@ function handleString(key: string, value: string): boolean {
 
 ;(async () => {
   try {
-    const yamlFilePath = core.getInput("yaml-file")
-    const yamlFile = fs.readFileSync(yamlFilePath, "utf8")
-    const multiDoc = core.getBooleanInput("multidoc")
+    const yamlFilePath = core.getInput('yaml-file')
+    const yamlFile = fs.readFileSync(yamlFilePath, 'utf8')
+    const multiDoc = core.getBooleanInput('multidoc')
     let yamlParse
     if (multiDoc) {
       console.log(`***** Output Variables *****`)
